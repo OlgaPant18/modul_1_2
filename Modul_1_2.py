730b930d6665b20eab35a694e3eedd264af9d67b
OlgaPant18 <opb2016@yandex.ru>
2024-08-12 13:55:41 +0700

    Add files via upload

---
+++ Modul_1_2.py
@@ -0,0 +1,5 @@
+number_of_completed_homework = 12
+number_of_hours_spent = 1.5
+the_name_of_the_class = "Python"
+time_for_one_homework = number_of_hours_spent / number_of_completed_homework
+print ("Курс: " + the_name_of_the_class, "всего задач:", number_of_completed_homework, "затрачено часов:", number_of_hours_spent, "среднее время выполнения: ", time_for_one_homework)
